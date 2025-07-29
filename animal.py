e8de541066690598c6f45d945c2bd1567ff1404a
glin05 <ginalin4409@gmail.com>
2025-07-29 10:56:11 -0400

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,16 @@
+"""
+This program prompts the user to enter their favorite animal
+and favorite color and then prints a related message.
+
+Author: Gina Lin
+Date: 01/28/2025
+Lecture section (delete the two that do not apply): 8:30 
+Lab section (delete the three that do not apply): T2:45 
+"""
+
+def main():
+    animal = input("What is your favorite animal? ")
+    print(animal)
+    color = input("What is your favorite color?")
+    print(color)
+main()
