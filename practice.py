3fe949e6ab24dc156b946eb45d43ab6675e0d71a
glin05 <ginalin4409@gmail.com>
2025-07-29 10:25:43 -0400

    Lab 1

---
+++ practice.py
@@ -0,0 +1,153 @@
+"""
+This is a sample Python program with some things to fix in it.
+It's your first program in Computer Science! Read through the file
+looking for the comment lines -- ones that start with a hash tag (#)
+-- and try to do what they ask. Don't worry if you mess up the file:
+it's only for practice!
+
+CTRL-x means hold the control key while also pressing the x key.
+
+"""
+
+def main():
+  # First: go to line 125 by pressing CTRL-g and entering 125.  You'll
+  # find a message there, which you can copy with CTRL-c and paste on
+  # line 17 with CTRL-v.
+  print("Welcome to CS21!")
+  print("The secret message is:CS is Fun")
+
+
+  # The line below is a mess. Delete it by highlighting the line
+  # with your mouse and pressing the backspace key.
+  
+
+  # The two "print" lines below need to be indented. Highlight these
+  # two lines by dragging the mouse across BOTH of the lines, then
+  # press Tab to indent them.
+  for num in range(10):
+    print(num)
+    print("--------------")
+
+
+  # If you ever want to undo or redo an edit, use:
+  # CTRL-z (undo) or CTRL-SHIFT-z (redo).
+  
+
+
+  # There should currently be a small dot next to the name
+  # 'practice.py' in the file tabs along the top of the vscode window
+  # -- that means you have unsaved changes.  To save a file, press
+  # CTRL-s and the dot should disappear.
+
+
+  # These next lines are OK, but for now, we want to comment them all
+  # out.  You could put a # sign at the beginning of each line, but if
+  # you want to comment multiple lines at once, highlight all of the
+  # lines and press CTRL-/ (control + forward slash). You can also 
+  # uncomment them by repeating these steps (highlighting, CTRL-/).
+  # print("this line is OK")
+  # print("but comment me out")
+  # print("for now...")
+
+
+
+  # To cut a line, highlight it and press CTRL-x.  To paste what you
+  # cut, press CTRL-v. Cut this line, then go line 152 (CTRL-g) and
+  # paste it there with CTRL-v. Come back to line 57 when you're done.
+  
+
+
+  # Where is CompSci? Use CTRL-f to search the document and find where
+  # it appears. You can press ENTER to move to the next instance or
+  # SHIFT-ENTER to search backwards to the previous instance.
+
+  # Edit the next print statement to show how many times you found
+  # CompSci in this document.
+  print("I found CompSci X times.")
+  print("We love CompSci!")
+  print("We love CompSci!")
+  print("We love CompSci!")
+  print("We love CompSci!")
+
+
+  # Finally, use CTRL-s to save your changes.  After you save and quit, try
+  # running the file with this command in the terminal:
+  # python3 practice.py
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Super.  The secret message is:  CS is Fun
+# Now go back to line 17 (CTRL-g)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Paste the end line here:
+print("This is the end.")
+# Then, jump back to line 57
