692d84e6479342d93ffe1cc7eeb1fefb2ba8947e
glin05 <ginalin4409@gmail.com>
2025-07-29 11:11:30 -0400

    Create lab1_intro.py

---
+++ lab1_intro.py
@@ -0,0 +1,13 @@
+"""
+This is a sample python program
+
+Author: Gina Lin
+Date: 01/28/2025
+"""
+#define the main function
+def main():
+    name = input("What is your name? ")
+    print("Hello")
+    print(name)
+    print("Nice to meet you!")
+main()
